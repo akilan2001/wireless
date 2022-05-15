87cf8918c356d74a41e4d3dd357bc9d7f004e752
123-ECE <105166338+123-ECE@users.noreply.github.com>
2022-05-15 17:48:20 +0530

    Update bcd_adder.v

--- bcd_adder.v
+++ bcd_adder.v
@@ -26,26 +26,21 @@ module bcd_adder(
     input Cin,
     output reg Cout,
     output reg [3:0] Sum,
-    reg [4:0] sum_temp 
+   
     );
-    
+    reg [4:0] sum_temp ;
     always @(*)
     begin
     sum_temp=A+B+Cin;
     if(sum_temp>9)
     begin
     sum_temp=sum_temp+4'b0110;
-    Sum=sum_temp[3:0];
-    Cout=1;
+        Sum=sum_temp[4:0];
     end
     else
-    begin
-    Cout=0;
-    Sum=sum_temp[3:0];
-    Cout=0;
+    {Cout,Sum}=sum_temp[4:0];
     end
     
-    end
     
     
 endmodule
