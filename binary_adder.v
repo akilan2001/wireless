5e689f33e19643d16fc0a386a66b5fa15a0b9e87
123-ECE <105166338+123-ECE@users.noreply.github.com>
2022-05-15 17:53:18 +0530

    Update binary_adder.v

--- binary_adder.v
+++ binary_adder.v
@@ -7,16 +7,13 @@ module binary_adder(
     input Cin,
     output [3:0] Sum,
     output Cout,
-    integer i,
-    reg prev_cout,reg [4:0] c_out_wire=0
+   
     );
-
+    wire [3:0] c_out_wire;
     full_adder f1(Sum[0],c_out_wire[1],A[0],B[0],Cin);
     full_adder f2(Sum[1],c_out_wire[2],A[1],B[1],c_out_wire[1]);
     full_adder f3(Sum[2],c_out_wire[3],A[2],B[2],c_out_wire[2]);
     full_adder f4(Sum[3],c_out_wire[4],A[3],B[3],c_out_wire[3]);
-
-    assign Cout=c_out_wire[4];
 
 endmodule
 
